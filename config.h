8abbc8d63322e63355fa1e7e6407094b047ef1be
hari0943 <45346645+hari0943@users.noreply.github.com>
2021-04-21 12:04:19 +0530

    Add files via upload

--- config.h
+++ config.h
@@ -2,18 +2,16 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"envypn-13"
+	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-#include "/home/void/.cache/wal/colors-wal-dmenu.h"
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 10;
-static unsigned int lineheight = 0;         /* -h option; minimum height of a menu line     */
-
+static unsigned int lines      = 0;
+#include "/home/<USER>/.cache/wal/colors-wal-dmenu.h"
 /*
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
